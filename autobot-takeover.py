8a645a726ef00b709eceaf58bc2d98eedadb8639
com0tf3 <com0tf3@gmail.com>
2021-02-27 22:48:39 +0700

    add nuclei and fix 2000 byte

--- autobot-takeover.py
+++ autobot-takeover.py
@@ -1,40 +1,58 @@
 import os, requests, sys, time
 from datetime import datetime
 
-webhook = 'https://discord.com/api/webhooks/814389502608277524/d6s49TIDkhbca1FX2CPfVsyXNarKFIEcnpOkGOP090kpIjd-_hPPde4CvXQjqw-4P-fL'
+takeover = 'https://discord.com/api/webhooks/814389502608277524/d6s49TIDkhbca1FX2CPfVsyXNarKFIEcnpOkGOP090kpIjd-_hPPde4CvXQjqw-4P-fL'
+nuclei = 'https://discord.com/api/webhooks/815248081229709372/HQiNhX2LOLii4B7k49D9M67t7RnXBqqEpS9JCgimal9NL4zhlgEuVe9WN5GL3fKsW8g3'
+
 data = {
         "content": "test"
         }
 n = 40
 importan = ['[AZURE]']
+content = 'empty'
+
+if len(sys.argv) < 3:
+    print(f'{sys.argv[0]} takeover|nuclei file-result.txt') 
+    exit(1)
+url = sys.argv[1]
 
 with open(sys.argv[1], 'r') as fp:
     content = fp.read()
 
 if content:
     try:
-        content = content.split('\n')
-        content.remove('')
-        contents = [content[i:i+n] for i in range(0,len(content), n)]
-
+        contents = content.split('\n')
+        try:
+            contents.remove('')
+        except:
+            pass
+        
+        start = 0
+        end = 1
         with open('domain-takeover-bot.log', 'w+') as fp:
-            c = 0
-            for content in contents:
-                content = '\n'.join([c for c in content])
-                content = f'> **Block {c}**\n> **Date: {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}**\n' + content
-                c += 1
-                for i in importan:
-                    content = content.replace(i, f'**{i}**')
-                data["content"] = content
-                resp = requests.post(webhook, json=data)
+            count = 0
+            while end <= len(content):
+                if len(content[start:end]) < 2000 and n <= len(content):
+                    end += 1
+                    continue
 
-                if resp.status_code != 204:
-                    print(resp.status_code)
-                    print(resp.content)
-                    fp.write(resp.content.decode('utf-8')+"\n---"+content+"\n\n\n")
+                contents = content[start:end-1]
+                for con in contents:
+                    con = '\n'.join([s for s in con])
+                    con = f'> **Block {count}**\n> **Date: {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}**\n' + con
+                    count += 1
+                    for i in importan:
+                        content = content.replace(i, f'**{i}**')
+                    data["content"] = con
+                    resp = requests.post(url, json=data)
 
-                if (c+9)/30 == 0:
-                    time.sleep(60)
+                    if resp.status_code != 204:
+                        print(resp.status_code)
+                        print(resp.content)
+                        fp.write(resp.content.decode('utf-8')+"\n---"+con+"\n\n\n")
+
+                    if (count+9)/30 == 0:
+                        time.sleep(60)
 
     except Exception as e:
         print(e)
